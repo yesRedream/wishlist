7cfa817acb8c1d0c773e04f09fd25e13bd3517f2
yesRedream <yesredream@gmail.com>
2020-09-22 20:59:00 +0300

    firebase host/bugfix

---
+++ build/precache-manifest.5e67bf9ea70f1aa3bc0bf32c4167d1ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e316a0bfd3efd8b749db470752fb6b61",
+    "url": "/wishlist/index.html"
+  },
+  {
+    "revision": "16f419ecbf81b049bcf5",
+    "url": "/wishlist/static/css/main.9e000273.chunk.css"
+  },
+  {
+    "revision": "893c1f754656fb3ab268",
+    "url": "/wishlist/static/js/2.9b59fd25.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/wishlist/static/js/2.9b59fd25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16f419ecbf81b049bcf5",
+    "url": "/wishlist/static/js/main.223e5a54.chunk.js"
+  },
+  {
+    "revision": "d8d234b8f6bcd8f1d57b",
+    "url": "/wishlist/static/js/runtime-main.08712abb.js"
+  }
+]);
