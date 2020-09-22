b14124bfa9c737ce09f64da6ab6aaac0f2bccffd
yesRedream <yesredream@gmail.com>
2020-09-22 15:56:30 +0300

    Updates

---
+++ precache-manifest.5752f13ef75d2b739f92ae4c97bb9b98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f337e825b93a9ea034a5444b1dce4c",
+    "url": "/wishlist/index.html"
+  },
+  {
+    "revision": "9a327cce80283c151aa9",
+    "url": "/wishlist/static/css/main.38f4a3ff.chunk.css"
+  },
+  {
+    "revision": "893c1f754656fb3ab268",
+    "url": "/wishlist/static/js/2.9b59fd25.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/wishlist/static/js/2.9b59fd25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a327cce80283c151aa9",
+    "url": "/wishlist/static/js/main.223e5a54.chunk.js"
+  },
+  {
+    "revision": "d8d234b8f6bcd8f1d57b",
+    "url": "/wishlist/static/js/runtime-main.08712abb.js"
+  }
+]);
